6dc0727ec0132367689c46992e8717d273845785
Charles Martin <cpm@charlesmartin.com.au>
2017-12-12 13:00:28 +0100

    Finish up the myo OSC script.

--- test_myo.py
+++ test_myo.py
@@ -2,6 +2,7 @@
 Connects to a Myo, then sends EMG and IMU data as OSC messages to localhost:3000
 """
 from myo_raw import *
+import math
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
@@ -17,26 +18,35 @@ def proc_imu(quat, acc, gyro):
     osc_client.send_message("/quat", proc_quat)
     osc_client.send_message("/acc", proc_acc)
     osc_client.send_message("/gyro", proc_gyro)
+    roll, pitch, yaw = toEulerAngle(proc_quat[0], proc_quat[1], proc_quat[2], proc_quat[3])
+    osc_client.send_message("/euler", (roll / math.pi, pitch / math.pi, yaw / math.pi))  # vals sent in [-1,1] (not [-pi,pi])
+    osc_client.send_message("/accmag", vector_3d_magnitude(proc_acc[0], proc_acc[1], proc_acc[2]))  # magnitude of accelerometer vector
+    osc_client.send_message("/gyrmag", vector_3d_magnitude(proc_gyro[0], proc_gyro[1], proc_gyro[2]))  # magnitude of gyroscope vector
+
 
 def proc_emg(emg_data):
-    #print("emg:", em_data, end='\r')
+    proc_emg = tuple(map(lambda x: x / 127.0, emg_data))  # scale EMG to be in [-1, 1]
+    # print("emg:", em_data, end='\r')
     osc_client.send_message("/emg", emg_data)
 
 
 def proc_battery(battery_level):
-    #print("Battery", battery_level, end='\r')
+    # print("Battery", battery_level, end='\r')
     osc_client.send_message("/battery", battery_level)
 
 
+# Setup Myo Connection
 m = MyoRaw(tty="/dev/tty.usbmodem1")
 m.add_emg_handler(proc_emg)
 m.add_imu_handler(proc_imu)
 m.add_battery_handler(proc_battery)
-m.connect()
+m.connect()  # connects to first Myo seen.
 
-# Setup Myo.
+# Setup Myo mode, buzzes when ready.
 m.sleep_mode(Sleep_Mode.never_sleep.value)
+# EMG and IMU are enabled, classifier is disabled (thus, no sync gestures required, less annoying buzzing).
 m.set_mode(EMG_Mode.send_emg.value, IMU_Mode.send_data.value, Classifier_Mode.disabled.value)
+# Buzz to show Myo is ready.
 m.vibrate(1)
 
 print("Now running...")
@@ -49,6 +59,30 @@ finally:
     m.disconnect()
     print("Disconnected")
 
+
+def vector_3d_magnitude(x, y, z):
+    """Calculate the magnitude of a 3d vector"""
+    return math.sqrt((x * x) + (y * y) + (z * z))
+
+
+def toEulerAngle(w, x, y, z):
+    """ Quaternion to Euler angle conversion borrowed from wikipedia.
+        https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles """
+    # roll (x-axis rotation)
+    sinr = +2.0 * (w * x + y * z)
+    cosr = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(sinr, cosr)
+    # pitch (y-axis rotation)
+    sinp = +2.0 * (w * y - z * x)
+    if (math.fabs(sinp) >= 1):
+        pitch = math.copysign(math.pi / 2, sinp)  # use 90 degrees if out of range
+    else:
+        pitch = math.asin(sinp)
+    # yaw (z-axis rotation)
+    siny = +2.0 * (w * z + x * y)
+    cosy = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(siny, cosy)
+    return roll, pitch, yaw
 
 # TODO:
 #   - direct connection to a specific myo.
